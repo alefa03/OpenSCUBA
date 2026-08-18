7dc832530e58008db8f4d1031176d87021351a99
alefa03 <nintendoads@gmail.com>
2026-08-19 01:50:16 +0200

    tween function bug fix

--- utils/tweener.js
+++ utils/tweener.js
@@ -1,14 +1,14 @@
 import * as TWEEN from '@tweenjs/tween.js';
 
 export class Tweener { // Utility class to handily perform tweening with Tween.js
-    static Easing = TWEEN.Easing; // re-exposed so callers don't need their own TWEEN import
+    static Easing = TWEEN.Easing;
 
     constructor() {
         throw new Error('Tweener is an abstract utility class and cannot be instantiated.');
     }
 
     static tween(target, to, duration = 1000, easing = TWEEN.Easing.Linear.None, onUpdate = null) {
-        const tween = new TWEEN.Tween(target)
+        const tween = new TWEEN.Tween(target, true)
             .to(to, duration)
             .easing(easing);
 
