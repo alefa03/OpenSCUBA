71e2b9953b8d08d1c70bc01706d5da0ade5b64d4
alefa03 <nintendoads@gmail.com>
2026-08-24 01:40:39 +0200

    deprecations fix

--- utils/tweener.js
+++ utils/tweener.js
@@ -2,13 +2,14 @@ import * as TWEEN from '@tweenjs/tween.js';
 
 export class Tweener { // Utility class to handily perform tweening with Tween.js
     static Easing = TWEEN.Easing;
+    static #group = new TWEEN.Group();
 
     constructor() {
         throw new Error('Tweener is an abstract utility class and cannot be instantiated.');
     }
 
     static tween(target, to, duration = 1000, easing = TWEEN.Easing.Linear.None, onUpdate = null) {
-        const tween = new TWEEN.Tween(target, true)
+        const tween = new TWEEN.Tween(target, Tweener.#group)
             .to(to, duration)
             .easing(easing);
 
@@ -21,6 +22,6 @@ export class Tweener { // Utility class to handily perform tweening with Tween.j
     }
 
     static update(time = performance.now()) {
-        return TWEEN.update(time);
+        return Tweener.#group.update(time);
     }
 }
